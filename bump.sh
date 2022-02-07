c57ceb79c3e4c6c73d1656bed5436d2e574e188d
Erik Pukinskis <erik@snowedin.net>
2022-02-07 06:36:25 +0000

    add bump

---
+++ bump.sh
@@ -0,0 +1,10 @@
+
+    #/bin/bash
+    git reset
+    git add package.json
+    git commit -m "v`npm version minor`"
+    npx json -f package.json -I -e "delete this.devDependencies"
+    npm publish --access public
+    git checkout -- package.json
+    git push
+  
