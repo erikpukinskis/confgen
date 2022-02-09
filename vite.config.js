3d447e4209773706727ac070c63b477d5c710368
Erik Pukinskis <erik@snowedin.net>
2022-02-09 06:06:32 +0000

    Add prettier and vite build

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import path from "path"
+import { defineConfig } from "vite"
+import commonjsExternals from "vite-plugin-commonjs-externals"
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  plugins: [
+    commonjsExternals({
+      "externals": ["fs", "child_process"],
+    }),
+  ],
+
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "Configgen",
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["fs-extra", "lodash", "merge-objects"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          "fs-extra": "fsextra",
+          "lodash": "lodash",
+          "merge-objects": "mergeobjects",
+        },
+      },
+    },
+  },
+})
