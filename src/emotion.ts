7834c09577f0b9ff5d4479512251bce4381c5d6a
Erik Pukinskis <erik@snowedin.net>
2022-02-11 01:58:42 +0000

    Don't need anything in base emotion config

--- src/emotion.ts
+++ src/emotion.ts
@@ -1,13 +1,3 @@
 import { CommandGenerator } from "./types"
 
-export const emotion: CommandGenerator = (presets) => [
-  {
-    command: "yarn",
-    pkg: "emotion",
-  },
-  ...(presets.includes("vite")
-    ? ([
-        { "command": "yarn", dev: true, pkg: "vite-plugin-babel-macros" },
-      ] as const)
-    : []),
-]
+export const emotion: CommandGenerator = (presets) => []
