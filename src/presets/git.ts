3841d1134def572d7deab1f9e4297cbe2e8c3583
Erik Pukinskis <erik@snowedin.net>
2022-02-21 10:44:52 -0700

    yarn preset puts yarn-error.log in .gitignore

--- src/presets/git.ts
+++ src/presets/git.ts
@@ -1,9 +1,18 @@
 import { CommandGenerator } from "@/types"
 
-export const git: CommandGenerator = () => [
+export const git: CommandGenerator = (presets) => [
   {
     command: "file",
     path: ".gitignore",
     contents: ["node_modules", ".DS_Store", "dist", "vendor"],
   },
+  ...(presets.includes("yarn")
+    ? [
+        {
+          command: "file" as const,
+          path: ".gitignore",
+          contents: ["yarn-error.log"],
+        },
+      ]
+    : []),
 ]
