ac49d1deb73335a72a0727e1a6a2bbfa71c7b895
Erik Pukinskis <erik@snowedin.net>
2022-02-25 00:40:25 -0700

    Oops, es2017 has to be lowercase, and an array!

--- src/presets/typescript.ts
+++ src/presets/typescript.ts
@@ -27,7 +27,7 @@ export const typescript: CommandGenerator = (presets) => [
     path: "tsconfig.json",
     contents: {
       compilerOptions: {
-        lib: "ES2017",
+        lib: ["es2017"],
         esModuleInterop: true,
         forceConsistentCasingInFileNames: true,
         strict: true,
