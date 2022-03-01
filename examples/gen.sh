3c15ae6c5569b853ab5bcb4ac11f4518add4e5ad
Erik Pukinskis <erik@snowedin.net>
2022-03-01 11:16:48 -0700

    Update gen:examples a bit

--- examples/gen.sh
+++ examples/gen.sh
@@ -1,28 +1,51 @@
 #/bin/bash
-cd examples/backend-lib
+cd examples
+rm -rf backend-lib/*
 echo "----------------------------------------
 
 backend-lib example
 
 ----------------------------------------
 "
+cd backend-lib
+echo '{
+  "scripts": {
+    "confgen": "npx ts-node --transpile-only ../../src/index.ts vite typescript library:BackendLib node:fs apollo:server"
+  }
+}' > package.json
 yarn confgen
 yarn all the things!
-cd ../frontend-lib
+
+exit 0
+cd ../
+rm -rf frontend-lib/*
 echo "----------------------------------------
 
 frontend-lib example
 
 ----------------------------------------
 "
+cd frontend-lib
+echo '{
+  "scripts": {
+    "confgen": "npx ts-node --transpile-only ../../src/index.ts vite react library:FrontendLib"
+  }
+}' > package.json
 yarn confgen
 yarn all the things!
-cd ../frontend-app
+cd ../
+rm -rf frontend-app/*
 echo "----------------------------------------
 
 frontend-app example
 
 ----------------------------------------
 "
+cd frontend-app
+echo '{
+  "scripts": {
+    "confgen": "npx ts-node --transpile-only ../../src/index.ts yarn codespaces vite vitest react library:FrontendLib apollo:client"
+  }
+}' > package.json
 yarn confgen
 yarn all the things!
