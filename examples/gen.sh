298486996f74b21d07a666fe94926ac7a6d1a78f
Erik Pukinskis <erik@snowedin.net>
2022-03-03 17:51:40 -0700

    Re-enable all the examples

--- examples/gen.sh
+++ examples/gen.sh
@@ -1,38 +1,38 @@
 #/bin/bash
 cd examples
-# rm -rf backend-lib/*
-# echo "----------------------------------------
+rm -rf backend-lib/*
+echo "----------------------------------------
 
-# backend-lib example
+backend-lib example
 
-# ----------------------------------------
-# "
-# cd backend-lib
-# echo '{
-#   "scripts": {
-#     "confgen": "npx ts-node --transpile-only ../../src/index.ts vite typescript library:BackendLib node:fs codegen:resolvers:schema eslint prettier"
-#   }
-# }' > package.json
-# yarn confgen
-# yarn all the things!
+----------------------------------------
+"
+cd backend-lib
+echo '{
+  "scripts": {
+    "confgen": "npx ts-node --transpile-only ../../src/index.ts vite typescript library:BackendLib node:fs codegen:resolvers:schema eslint prettier"
+  }
+}' > package.json
+yarn confgen
+yarn all the things!
 
-# cd ../
-# rm -rf frontend-lib/*
-# echo "----------------------------------------
+cd ../
+rm -rf frontend-lib/*
+echo "----------------------------------------
 
-# frontend-lib example
+frontend-lib example
 
-# ----------------------------------------
-# "
-# cd frontend-lib
-# echo '{
-#   "scripts": {
-#     "confgen": "npx ts-node --transpile-only ../../src/index.ts vite react macros typescript:tsconfig.build.json library:FrontendLib:development react devServer:docs eslint prettier"
-#   }
-# }' > package.json
-# yarn confgen
-# yarn all the things!
-# cd ../
+----------------------------------------
+"
+cd frontend-lib
+echo '{
+  "scripts": {
+    "confgen": "npx ts-node --transpile-only ../../src/index.ts vite react macros typescript:tsconfig.build.json library:FrontendLib:development react devServer:docs eslint prettier"
+  }
+}' > package.json
+yarn confgen
+yarn all the things!
+cd ../
 rm -rf frontend-app/*
 echo "----------------------------------------
 
