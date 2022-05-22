f0fb81cc6e224e33e941eee2a0b69c8e46c4edaf
Erik Pukinskis <erik@snowedin.net>
2022-05-22 19:41:43 +0000

    fix path import in vite configs

--- src/presets/vite.ts
+++ src/presets/vite.ts
@@ -157,7 +157,7 @@ const buildViteConfig = (presets: Presets, args: Args, system: System) => {
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "src", "index.html"),
+        main: path.resolve(__dirname, "src", "index.html"),
       },
     },
   },
