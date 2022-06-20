2426a645102642c8d966ca7e71d922e4199ca36f
Erik Pukinskis <erik@snowedin.net>
2022-06-20 21:12:46 +0000

    Error message

--- src/presets/dist.ts
+++ src/presets/dist.ts
@@ -7,7 +7,11 @@ import type {
 } from "@/commands"
 import { isBuild } from "@/builds"
 
-export const precheck: Precheck = ({ args }) => {
+export const precheck: Precheck = ({ args, presets }) => {
+  if (!presets.includes("vite")) {
+    throw new Error("Cannot use the dist preset without the vite preset")
+  }
+
   if (args.dist.length < 1) {
     throw new Error(
       "[dist] preset requires at least one build.\n\nTry dist:lib:app"
