bc189dce03f562bc185778017d3fb3eb40b5b2f0
Erik Pukinskis <erik@snowedin.net>
2022-06-25 16:53:04 +0000

    Make a separate bin.sh instead of prepending a #! to the JavaScript

--- src/presets/bin.ts
+++ src/presets/bin.ts
@@ -5,13 +5,13 @@ export const generator: CommandGenerator = () => [
     command: "script",
     name: "build:bin",
     script:
-      "echo '#!/usr/bin/env node'|cat - dist/index.umd.js > /tmp/out && mv /tmp/out dist/index.umd.js && chmod a+x dist/index.umd.js",
+      "echo '#!/bin/bash\n/usr/bin/env node $(dirname -- $0)/index.umd.js $@\n' > dist/bin.sh && chmod a+x dist/bin.sh",
   },
   {
     command: "file",
     path: "package.json",
     contents: {
-      bin: "./dist/index.umd.js",
+      bin: "./dist/bin.sh",
     },
   },
 ]
