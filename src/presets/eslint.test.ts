361f215f157c39916a4b6d1c2d483ed4c2bdf8c9
Erik Pukinskis <erik@snowedin.net>
2022-06-29 18:59:56 +0000

    Move react+eslint.test.ts to just eslint.test.ts

---
+++ src/presets/eslint.test.ts
@@ -0,0 +1,40 @@
+import { describe, it, expect, beforeEach } from "vitest"
+import { Project } from "../project"
+import { MockSystem, type System } from "../system"
+
+describe("presets/eslint", () => {
+  describe("without the react preset", () => {
+    let system: System
+
+    beforeEach(async () => {
+      system = new MockSystem()
+      const project = new Project({
+        builds: ["app"],
+        presetConfigs: ["eslint"],
+        system,
+      })
+      await project.confgen()
+    })
+
+    it("should not have the eslint-plugin-react package", () => {
+      expect(system.read("package.json")).not.toContain("eslint-plugin-react")
+    })
+  })
+
+  describe("with the react preset", () => {
+    let system: System
+
+    beforeEach(async () => {
+      const project = new Project({
+        builds: ["app"],
+        presetConfigs: ["eslint", "react"],
+        system,
+      })
+      await project.confgen()
+    })
+
+    it("should have the eslint-plugin-react package", () => {
+      expect(system.read("package.json")).toContain("eslint-plugin-react")
+    })
+  })
+})
