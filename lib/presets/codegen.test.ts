a1759ce39c56639d7438d75ff30142a57491cf19
Erik Pukinskis <erik@snowedin.net>
2022-09-15 05:35:24 +0000

    Fix splat of generators in codegen preset

--- lib/presets/codegen.test.ts
+++ lib/presets/codegen.test.ts
@@ -3,7 +3,7 @@ import { Project } from "@/project"
 import { MockSystem } from "@/system"
 
 describe("presets/codegen", () => {
-  it("should throw an error without typescript", () => {
+  it("should throw an error without typescript", async () => {
     const system = new MockSystem()
     const project = new Project({
       builds: ["app"],
@@ -11,8 +11,21 @@ describe("presets/codegen", () => {
       system,
     })
 
-    void expect(async () => await project.confgen()).rejects.toThrowError(
+    await expect(async () => await project.confgen()).rejects.toThrowError(
       /GraphQL codegen only makes sense in a Typescript project/
     )
+  })
+
+  it("should write a codegen file", async () => {
+    const system = new MockSystem()
+    const project = new Project({
+      builds: ["app"],
+      presetConfigs: ["typescript", "codegen:lib:resolvers:schema"],
+      system,
+    })
+
+    await project.confgen()
+
+    expect(system.exists("codegen.yml")).toBe(true)
   })
 })
