8b3d3b29bf5434d432a8a8c94462e6e42f0e25f5
Erik Pukinskis <erik@snowedin.net>
2022-10-09 00:23:49 +0000

    Add separate tsconfig.build.json

--- lib/presets/typescript.ts
+++ lib/presets/typescript.ts
@@ -1,4 +1,4 @@
-import type { CommandGenerator, Precheck, Builds } from "@/commands"
+import type { CommandGenerator, Precheck, Builds, Presets } from "@/commands"
 
 export const precheck: Precheck = ({ args }) => {
   if (args.typescript.length > 0) {
@@ -22,9 +22,14 @@ export const generator: CommandGenerator = ({ presets, builds }) => [
           pkg: "tsc-alias",
         },
         {
+          command: "file",
+          path: "tsconfig.dist.json",
+          contents: buildDistConfig(),
+        },
+        {
           command: "script",
           name: "build:types",
-          script: `tsc --declaration --emitDeclarationOnly -p ${tsconfigPath()} --skipLibCheck && tsc-alias -p ${tsconfigPath()} && mv dist/index.d.ts dist/index.umd.d.ts`,
+          script: `tsc --declaration --emitDeclarationOnly -p tsconfig.dist.json --skipLibCheck && tsc-alias -p ${tsconfigPath()} && mv dist/index.d.ts dist/index.umd.d.ts`,
         },
       ] as const)
     : []),
@@ -36,21 +41,7 @@ export const generator: CommandGenerator = ({ presets, builds }) => [
   {
     command: "file",
     path: tsconfigPath(),
-    contents: {
-      compilerOptions: {
-        lib: ["es2017", ...(presets.includes("react") ? ["dom"] : [])],
-        baseUrl: ".",
-        paths: buildPathAliases(builds),
-        esModuleInterop: true,
-        forceConsistentCasingInFileNames: true,
-        strict: true,
-        skipLibCheck: true,
-        downlevelIteration: true,
-        ...(presets.includes("react") ? { jsx: "react" } : undefined),
-        ...(presets.includes("dist") ? { outDir: "dist" } : undefined),
-      },
-      include: builds,
-    },
+    contents: buildConfig(presets, builds),
   },
 ]
 
@@ -66,4 +57,28 @@ export const generator: CommandGenerator = ({ presets, builds }) => [
  */
 const buildPathAliases = (builds: Builds) => ({
   "@/*": [`${builds[0]}/*`],
+})
+
+/**
+ * Special config for just building lib.
+ */
+const buildDistConfig = () => ({
+  extends: "./tsconfig.json",
+  include: ["lib"],
+})
+
+const buildConfig = (presets: Presets, builds: Builds) => ({
+  compilerOptions: {
+    lib: ["es2017", ...(presets.includes("react") ? ["dom"] : [])],
+    baseUrl: ".",
+    paths: buildPathAliases(builds),
+    esModuleInterop: true,
+    forceConsistentCasingInFileNames: true,
+    strict: true,
+    skipLibCheck: true,
+    downlevelIteration: true,
+    ...(presets.includes("react") ? { jsx: "react" } : undefined),
+    ...(presets.includes("dist") ? { outDir: "dist" } : undefined),
+  },
+  include: builds,
 })
