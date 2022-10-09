9aaa2bbfc006de3440858a2001093cdb8483c3be
Erik Pukinskis <erik@snowedin.net>
2022-10-09 18:42:13 -0400

    Fix test file detection so it works on MacOS

--- lib/presets/vitest.ts
+++ lib/presets/vitest.ts
@@ -1,74 +1,82 @@
-import type { CommandGenerator, System, Presets } from "@/commands"
+import type {
+  CommandGenerator,
+  System,
+  Presets,
+  CommandWithArgs,
+} from "@/commands"
 import type { Build } from "@/builds"
 
-export const generator: CommandGenerator = ({ builds, presets, system }) => [
-  {
-    command: "yarn",
-    dev: true,
-    pkg: "vitest",
-  },
-  {
-    command: "script",
-    name: "test",
-    script: "vitest run",
-  },
-  {
-    command: "script",
-    name: "test:watch",
-    script: "vitest watch",
-  },
-  ...(presets.includes("codespaces")
-    ? ([
-        {
-          command: "file",
-          path: ".vscode/launch.json",
-          contents: {
-            version: "0.2.0",
-            configurations: [
-              {
-                type: "pwa-node",
-                request: "launch",
-                name: "Debug Current Test File",
-                skipFiles: ["<node_internals>/**", "**/node_modules/**"],
-                program: "${workspaceRoot}/node_modules/vitest/vitest.mjs",
-                args: ["run", "${relativeFile}"],
-                smartStep: true,
-                console: "integratedTerminal",
-              },
-            ],
+export const generator: CommandGenerator = ({ builds, presets, system }) => {
+  const commands: CommandWithArgs[] = [
+    {
+      command: "yarn",
+      dev: true,
+      pkg: "vitest",
+    },
+    {
+      command: "script",
+      name: "test",
+      script: "vitest run",
+    },
+    {
+      command: "script",
+      name: "test:watch",
+      script: "vitest watch",
+    },
+  ]
+
+  if (presets.includes("codespaces")) {
+    commands.push({
+      command: "file",
+      path: ".vscode/launch.json",
+      contents: {
+        version: "0.2.0",
+        configurations: [
+          {
+            type: "pwa-node",
+            request: "launch",
+            name: "Debug Current Test File",
+            skipFiles: ["<node_internals>/**", "**/node_modules/**"],
+            program: "${workspaceRoot}/node_modules/vitest/vitest.mjs",
+            args: ["run", "${relativeFile}"],
+            smartStep: true,
+            console: "integratedTerminal",
           },
-        },
-      ] as const)
-    : []),
-  ...(!hasTestFiles(system)
-    ? ([
-        {
-          command: "file",
-          path: buildExampleTestPath(builds[0], presets),
-          merge: "if-not-exists",
-          contents: buildExampleTest(builds[0], presets),
-        },
-      ] as const)
-    : []),
-  ...(presets.includes("react")
-    ? ([
-        {
-          command: "yarn",
-          pkg: "@testing-library/react",
-          dev: true,
-        },
-        {
-          command: "yarn",
-          pkg: "react-dom",
-          dev: true,
-        },
-      ] as const)
-    : []),
-]
+        ],
+      },
+    })
+  }
+
+  if (!hasTestFiles(system)) {
+    commands.push({
+      command: "file",
+      path: buildExampleTestPath(builds[0], presets),
+      merge: "if-not-exists",
+      contents: buildExampleTest(builds[0], presets),
+    })
+  }
+
+  if (presets.includes("react")) {
+    commands.push(
+      {
+        command: "yarn",
+        pkg: "@testing-library/react",
+        dev: true,
+      },
+      {
+        command: "yarn",
+        pkg: "react-dom",
+        dev: true,
+      }
+    )
+  }
+
+  return commands
+}
 
 const hasTestFiles = (system: System) => {
   const { status } = system.run(
-    `find . -regex '^.+[.]test[.][tj]sx?$' -not -path "./node_modules/*" | grep .`
+    `find . -not -path "./node_modules/*" | grep -E '^.+[.]test[.][tj]sx?$' | grep .`
   )
   return status === 0
 }
